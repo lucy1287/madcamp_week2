03f4336704dbbcebb4910c25707e56877c39e530
lucy1287 <lucy1287@naver.com>
2024-07-10 04:01:17 +0900

    Feat: 내 일정 조회

--- controllers/plan.js
+++ controllers/plan.js
@@ -1,4 +1,4 @@
-const {Plan} = require("../models/associations");
+const { Plan, Concert } = require("../models/associations");
 
 exports.createPlan = async function(req, user_id, res) {
     try {
@@ -8,6 +8,9 @@ exports.createPlan = async function(req, user_id, res) {
         let startDateString = planData.date;
         if (startDateString.includes('~')) {
             startDateString = startDateString.split('~')[0].trim();
+        }
+        else if (startDateString.includes('-')) {
+            startDateString = startDateString.split('-')[0].trim();
         }
         const startDate = new Date(startDateString);
 
@@ -24,5 +27,24 @@ exports.createPlan = async function(req, user_id, res) {
 
     } catch (err) {
         res.status(500).json({ message: '일정 생성 중 오류가 발생했습니다.', error: err.message });
+    }
+};
+
+exports.getPlansByUserId = async function(id) {
+    try {
+        const plans = await Plan.findAll({
+            attributes: ['date'],
+            include: [{
+                model: Concert,
+                as: 'concert',
+                attributes: ['title', 'place', 'date', 'image_url'],
+                required: true // INNER JOIN으로 조인
+            }],
+            where: { user_id: id }
+        });
+        return plans;
+    } catch (err) {
+        console.error('일정 조회 중 오류 발생:', err);
+        throw new Error('해당 사용자의 일정 조회 중 오류가 발생했습니다.');
     }
 };
--- routes/plan.js
+++ routes/plan.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const plan = require("../controllers/plan");
+const review = require("../controllers/review");
 
 router.post('/create/:user_id', async function(req, res) {
 
@@ -13,6 +14,18 @@ router.post('/create/:user_id', async function(req, res) {
     } catch (error) {
         console.error(`에러 발생: ${error}`);
         res.status(500).send(`에러 발생: ${error}`);
+    }
+});
+
+// 리뷰 전체 보기 라우트
+router.get('/:user_id', async (req, res) => {
+    try {
+        let user_id = req.params.user_id;
+        const plans = await plan.getPlansByUserId(user_id);
+        res.status(200).json(plans);
+    } catch (error) {
+        console.error(`에러 발생: ${error}`);
+        res.status(500).json({ message: '일정 조회 중 오류가 발생했습니다.', error: error.message });
     }
 });
 
